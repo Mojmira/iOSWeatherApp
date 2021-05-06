49f2ef7c01199d77c40fc6230138376c77071b7f
mojmira <bymaxa@gmail.com>
2021-05-06 21:33:15 +0200

    init

---
+++ iOSWeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSWeatherApp
+//
+//  Created by Mojmira on 06/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, worldd!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSWeatherApp/iOSWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSWeatherAppApp.swift
+//  iOSWeatherApp
+//
+//  Created by Mojmira on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct iOSWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
