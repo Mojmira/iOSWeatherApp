b42a458d0d021efd2a92aab5794bd537eff165d0
mojmira <bymaxa@gmail.com>
2021-05-11 15:31:47 +0200

    Weather records

--- iOSWeatherApp/ContentView.swift
+++ iOSWeatherApp/ContentView.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, worldd!")
-            .padding()
+        VStack{
+            ForEach(1..<8){_ in
+                WeatherRecordView()
+            }
+        }.padding()
+        
     }
+}
+
+struct WeatherRecordView: View{
+    var body: some View{
+        ZStack{
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke()
+            HStack{
+                Text("☀️")
+                    .font(.largeTitle)
+                
+                VStack{
+                    Text("Krakow")
+                    Text("Temperature: 27℃")
+                        .font(.caption)
+                }
+            }
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
